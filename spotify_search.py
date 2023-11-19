478bd829bf199d183a90afb1d081337903b1bf51
Darth Demono <64983290+darthdemono@users.noreply.github.com>
2023-11-20 00:58:23 +0600

    Update spotify_search.py
    Added Error Handling. comments and other optimizations

--- spotify_search.py
+++ spotify_search.py
@@ -1,83 +1,106 @@
 import os
-from spotify_client import *
-from functions import *
-from dotenv import load_dotenv
 import json
+from spotify_client import SpotifyAPI
+from functions import ms_to_min, key_value, explicit_to_boolean, print_separator_line
+from dotenv import load_dotenv
+
 load_dotenv()
 
 spotify = SpotifyAPI(os.getenv("client_id"), os.getenv("client_secret"))
 
-album = input("Album: ")
-artist = input("Artist: ")
-explicit = explicit_to_boolean()
+def print_track_details(item, total_tracks):
+    """Print details for a track."""
+    print("_______________________________________________________________________")
+    # Extract relevant information from the track item
+    track_explicit = item["explicit"]
+    track_number = int(item["track_number"])
+    disc_number = int(item["disc_number"])
+    track_name = item["name"]
+    track_duration_ms = item["duration_ms"]
+    track_duration = ms_to_min(track_duration_ms)
+    album_type = item["album"]["album_type"]
+    track_external_ids = item["external_ids"]
+    release_date = item["album"]["release_date"]
+    track_link = item["external_urls"]["spotify"]
+    album_link = item["album"]["external_urls"]["spotify"]
+    artist_link = item["album"]["artists"][0]["external_urls"]["spotify"]
+    images = item["album"]["images"][0]["url"]
+    album_id = item["album"]["id"]
+    album_dict = spotify.get_resource("albums", f"{album_id}")
+    copyrights = album_dict["copyrights"][0]["text"]
+    album_external_ids = album_dict["external_ids"]
+    label = album_dict["label"]
+    artists = item["artists"]
+    track_artists = '; '.join(artist["name"] for artist in artists)
 
-track_dict = spotify.search({"album": album,"artist": artist},search_type="track")
+    # Print the extracted information
+    print(f"For Track Number {track_number}/{total_tracks}:")
+    print(f"Disc Number: {disc_number}")
+    print(f"Track Name: {track_name}")
+    print(f"Track Artists: {track_artists}")
+    print(f"Track Duration: {track_duration}")
+    print(f"Album Type: {album_type}")
+    print(f"Track Explicit: {track_explicit}")
+    print("External IDs: ")
+    for key, value in track_external_ids.items():
+        print(f"    {key_value(key, value)}")
+    for key, value in album_external_ids.items():
+        print(f"    {key_value(key, value)}")
+    print(f"Release Date: {release_date}")
+    print(f"Copyrights: {copyrights}")
+    print(f"Label: {label}")
+    print(f"Track Link: {track_link}")
+    print(f"Album link: {album_link}")
+    print(f"Artist Link: {artist_link}")
+    print(f"640px image: {images}")
 
-data = track_dict["tracks"]["items"]
-data.sort(key=lambda x: (x["disc_number"], x["track_number"]))
+def print_artist_details(artist_id):
+    """Print details for an artist."""
+    print_separator_line()
+    artist_dict = spotify.get_resource("artists", artist_id)
+    followers = artist_dict["followers"]["total"]
+    artist_genres = artist_dict["genres"]
+    print(f"Total Tracks: {total_tracks}")
+    print(f"Album Type: {album_type}")
+    print(f"Artist Followers: {followers}")
+    for genre in artist_genres:
+        print(f"Artist Genres: {genre}")
 
-total_tracks = int(data[0]["album"]["total_tracks"])
-album_id = data[0]["album"]["id"]
-artist_id = data[0]["artists"][0]["id"]
-album_type = data[0]["album"]["album_type"]
+try:
+    # Get user input for album, artist, and explicit filter
+    album = input("Album: ")
+    artist = input("Artist: ")
+    explicit = explicit_to_boolean()
 
-with open("data.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, sort_keys=True, ensure_ascii=True, indent=2)
+    # Search for tracks based on user input
+    track_dict = spotify.search({"album": album, "artist": artist}, search_type="track")
+    data = track_dict["tracks"]["items"]
 
-artist_dict = spotify.get_resource("artists", f"{artist_id}")
-followers = artist_dict["followers"]["total"]
-artist_genres = artist_dict["genres"]
+    if not data:
+        print_separator_line()
+        print("No tracks found for the given query.")
+    else:
+        # Sort tracks based on disc number and track number
+        data.sort(key=lambda x: (x["disc_number"], x["track_number"]))
+        total_tracks = int(data[0]["album"]["total_tracks"])
+        album_type = data[0]["album"]["album_type"]
+        # Filter tracks based on explicit filter
+        filtered_data = [item for item in data if item["explicit"] == explicit]
 
-print("_______________________________________________________________________")
-print(f"Total Tracks: {total_tracks}")
-print(f"Album Type: {album_type}")
-print(f"Artist Followers: {followers}")
-for genre in artist_genres:
-    print(f"Artist Genres: {genre}")
+        if not filtered_data:
+            print_separator_line()
+            print("No explicit tracks found for the given query.")
+        else:
+            # Print details for the first artist (assuming all tracks have the same artist)
+            print_artist_details(data[0]['artists'][0]['id'])
+            # Print details for each filtered track
+            for item in filtered_data:
+                try:
+                    print_track_details(item, total_tracks)
+                except KeyError as key_error:
+                    print(f"Error processing track: {key_error}")
+                except Exception as err:
+                    print(f"An error occurred while processing track: {err}")
 
-filtered_data = [item for item in data if item["explicit"] == explicit]
-
-for i, item in enumerate(filtered_data):
-    try:
-        print("_______________________________________________________________________")
-        track_explicit = item["explicit"]
-        track_number = int(item["track_number"])
-        disc_number = int(item["disc_number"])
-        track_name = item["name"]
-        track_duration_ms = item["duration_ms"]
-        track_duration = ms_to_min(track_duration_ms)
-        album_type = item["album"]["album_type"]
-        track_external_ids = item["external_ids"]
-        release_date = item["album"]["release_date"]
-        track_link = item["external_urls"]["spotify"]
-        album_link = item["album"]["external_urls"]["spotify"]
-        artist_link = item["album"]["artists"][0]["external_urls"]["spotify"]
-        images = item["album"]["images"][0]["url"]
-        album_id = item["album"]["id"]
-        album_dict = spotify.get_resource("albums", f"{album_id}")
-        copyrights = album_dict["copyrights"][0]["text"]
-        album_external_ids = album_dict["external_ids"]
-        label = album_dict["label"]
-        artists = item["artists"]
-        track_artists = '; '.join(artist["name"] for artist in artists)
-        print(f"For Track Number {track_number}/{total_tracks}:")
-        print(f"Disc Number: {disc_number}")
-        print(f"Track Name: {track_name}")
-        print(f"Track Artists: {track_artists}")
-        print(f"Track Duration: {track_duration}")
-        print(f"Album Type: {album_type}")
-        print(f"Track Explicit: {track_explicit}")
-        print("External IDs: ")
-        for key, value in track_external_ids.items():
-            print(f"    {key_value(key, value)}")
-        for key, value in album_external_ids.items():
-            print(f"    {key_value(key, value)}")
-        print(f"Release Date: {release_date}")
-        print(f"Copyrights: {copyrights}")
-        print(f"Label: {label}")
-        print(f"Track Link: {track_link}")
-        print(f"Album link: {album_link}")
-        print(f"Artist Link: {artist_link}")
-        print(f"640px image: {images}")
-    except Exception as err:
-        print(f"There Was an Error :{err}")
+except Exception as general_err:
+    print(f"An unexpected error occurred: {general_err}")
